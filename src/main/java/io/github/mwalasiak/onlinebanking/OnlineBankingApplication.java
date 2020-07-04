254dedb035714f0cc65da403382b206274c22a2f
Michał Walasiak <m.walasiak@gmail.com>
2020-07-04 17:22:10 +0200

    Initial commit

---
+++ src/main/java/io/github/mwalasiak/onlinebanking/OnlineBankingApplication.java
@@ -0,0 +1,13 @@
+package io.github.mwalasiak.onlinebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingApplication.class, args);
+	}
+
+}
