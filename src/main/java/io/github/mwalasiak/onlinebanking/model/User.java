9d1217abeb8f5799ba20684dee08ce57c49aa34d
Michał Walasiak <m.walasiak@gmail.com>
2020-07-14 13:07:21 +0200

    Add private fields

--- src/main/java/io/github/mwalasiak/onlinebanking/model/User.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/User.java
@@ -1,4 +1,17 @@
 package io.github.mwalasiak.onlinebanking.model;
 
 public class User {
+
+    private Integer userId;
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    private boolean enabled = true;
+
+
+
 }
