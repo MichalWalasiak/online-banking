2083db7261ab106ea1df1fc1eb66fc23011e6baa
Michał Walasiak <m.walasiak@gmail.com>
2020-07-14 13:12:22 +0200

    Add getters and setters

--- src/main/java/io/github/mwalasiak/onlinebanking/model/User.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/User.java
@@ -12,6 +12,67 @@ public class User {
 
     private boolean enabled = true;
 
+    public Integer getUserId() {
+        return userId;
+    }
 
+    public void setUserId(final Integer userId) {
+        this.userId = userId;
+    }
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(final String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(final String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(final boolean enabled) {
+        this.enabled = enabled;
+    }
 }
