05ea3cc4576792fbfe9dc0fc8e1418e755148845
Michał Walasiak <m.walasiak@gmail.com>
2020-07-14 13:44:13 +0200

    Add private fields

--- src/main/java/io/github/mwalasiak/onlinebanking/model/PrimaryTransaction.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/PrimaryTransaction.java
@@ -1,4 +1,15 @@
 package io.github.mwalasiak.onlinebanking.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class PrimaryTransaction {
+
+    private Integer id;
+    private Date date;
+    private String description;
+    private String type;
+    private String status;
+    private double amount;
+    private BigDecimal availableBalance;
 }
