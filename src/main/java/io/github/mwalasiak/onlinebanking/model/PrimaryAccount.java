87cdea7b722fe97a15ed949ffce013cc3f70ef85
Michał Walasiak <m.walasiak@gmail.com>
2020-07-14 13:32:55 +0200

    Add private fields to class

--- src/main/java/io/github/mwalasiak/onlinebanking/model/PrimaryAccount.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/PrimaryAccount.java
@@ -1,4 +1,15 @@
 package io.github.mwalasiak.onlinebanking.model;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PrimaryAccount {
+
+    private Integer id;
+    private int accountNumber;
+    private BigDecimal accountBalance;
+
+    private List<PrimaryTransaction> primaryTransactionList;
+
+
 }
