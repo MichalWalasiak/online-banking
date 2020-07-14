8f12e4890e48a061010cf24aacdfb2d8ea122fd7
Michał Walasiak <m.walasiak@gmail.com>
2020-07-14 13:34:17 +0200

    Add getters and setters

--- src/main/java/io/github/mwalasiak/onlinebanking/model/PrimaryAccount.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/PrimaryAccount.java
@@ -11,5 +11,35 @@ public class PrimaryAccount {
 
     private List<PrimaryTransaction> primaryTransactionList;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(final int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(final BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public List<PrimaryTransaction> getPrimaryTransactionList() {
+        return primaryTransactionList;
+    }
+
+    public void setPrimaryTransactionList(final List<PrimaryTransaction> primaryTransactionList) {
+        this.primaryTransactionList = primaryTransactionList;
+    }
 }
