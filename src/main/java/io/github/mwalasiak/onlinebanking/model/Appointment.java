2bf08ac5b9b971bcd66c6e1431019e756a94ddec
Michał Walasiak <m.walasiak@gmail.com>
2020-07-15 19:10:22 +0200

    Add private fields

--- src/main/java/io/github/mwalasiak/onlinebanking/model/Appointment.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/Appointment.java
@@ -1,4 +1,15 @@
 package io.github.mwalasiak.onlinebanking.model;
 
+import java.util.Date;
+
 public class Appointment {
+
+    private Integer id;
+    private Date date;
+    private String location;
+    private String description;
+    private boolean confirmed;
+
+    private User user;
+
 }
