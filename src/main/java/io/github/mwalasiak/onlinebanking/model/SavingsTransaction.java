98d791517bce4ee85aebff1ae2a87368f7e9c774
Michał Walasiak <m.walasiak@gmail.com>
2020-07-15 19:07:12 +0200

    Add getters and setters

--- src/main/java/io/github/mwalasiak/onlinebanking/model/SavingsTransaction.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/SavingsTransaction.java
@@ -24,4 +24,68 @@ public class SavingsTransaction {
         this.availableBalance = availableBalance;
         this.savingsAccount = savingsAccount;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(final Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(final String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(final String status) {
+        this.status = status;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(final double amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public void setAvailableBalance(final BigDecimal availableBalance) {
+        this.availableBalance = availableBalance;
+    }
+
+    public SavingsAccount getSavingsAccount() {
+        return savingsAccount;
+    }
+
+    public void setSavingsAccount(final SavingsAccount savingsAccount) {
+        this.savingsAccount = savingsAccount;
+    }
 }
