9f776836ad29e63260a55d852245f19d09a5cf74
Michał Walasiak <m.walasiak@gmail.com>
2020-07-15 19:11:51 +0200

    Add getters and setters

--- src/main/java/io/github/mwalasiak/onlinebanking/model/Appointment.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/Appointment.java
@@ -12,4 +12,51 @@ public class Appointment {
 
     private User user;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(final Date date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(final String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(final boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(final User user) {
+        this.user = user;
+    }
 }
