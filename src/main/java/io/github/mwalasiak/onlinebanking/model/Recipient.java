28d2610768352bd8fa0fe009d794706b7b6fdfd8
Michał Walasiak <m.walasiak@gmail.com>
2020-07-15 19:15:22 +0200

    Add private fields

--- src/main/java/io/github/mwalasiak/onlinebanking/model/Recipient.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/Recipient.java
@@ -1,4 +1,14 @@
 package io.github.mwalasiak.onlinebanking.model;
 
 public class Recipient {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private String accountNumber;
+    private String description;
+
+    private User user;
+
 }
