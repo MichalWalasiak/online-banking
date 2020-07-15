d2ec12258c5f9fafbb34784ec66b93c4b20b8f34
Michał Walasiak <m.walasiak@gmail.com>
2020-07-15 19:05:43 +0200

    Add private fields

--- src/main/java/io/github/mwalasiak/onlinebanking/model/SavingsTransaction.java
+++ src/main/java/io/github/mwalasiak/onlinebanking/model/SavingsTransaction.java
@@ -1,4 +1,18 @@
 package io.github.mwalasiak.onlinebanking.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class SavingsTransaction {
+
+    private Integer id;
+    private Date date;
+    private String description;
+    private String type;
+    private String status;
+    private double amount;
+    private BigDecimal availableBalance;
+
+    private SavingsAccount savingsAccount;
+
 }
